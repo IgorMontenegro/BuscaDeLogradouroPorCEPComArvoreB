0ce89f02978cd3a08a65602b77e8865c4163e700
IgorMontenegro <c.montenegro.c@hotmail.com>
2017-04-10 00:04:15 -0300

    Commit
    versão 3.0 com leitura direta, sem base binária e sem arvore
    implementada.

--- Release 2.0/main.c
+++ Release 2.0/main.c
@@ -1,82 +1,61 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <time.h>
-#include <limits.h>
-#include <io.h>
 #include "stdlib.h"
+#include "limits.h"
 #include "arvore_binaria.h"
 #define TAMANHO_ARRAY 2200
 
-int main(){
+int main(void){
 
     //Cria arvore e adiciona os dados do arquivo.
-	//char a[TAMANHO_ARRAY];
-    //ARVORE_B raiz;
-    char log;//resultado, ;
-    //int result;
-    int cep, buscaCEP;
+	char a[TAMANHO_ARRAY];
+    ARVORE_B raiz;
+	int i = 0;
+    char url[]="BaseDeCepsELogradourosBIN.txt",
+         linha,
+         resultado;
 
-	FILE * arq;
+    char entrada = "";
 
-	//Inicia tempo de leitura
-	clock_t begin = clock();
+	FILE *arq;
 
-	arq = fopen("NovaBase.txt", "r");
-
-	if(arq == NULL){
-			printf("Erro, nao foi possivel abrir o arquivo.\n");
-	}
+	arq = fopen(url, "r");
+	if(arq == NULL)
+			printf("Erro, nao foi possivel abrir o arquivo\n");
 	else
-    {
-        system ("Realize sua pesquisa abaixo:");
-        printf("### Pesquise algum CEP ###\n");
-        printf("Digite o CEP: ");
-        scanf("%i", &buscaCEP);
-
-		while( (fscanf(arq,"%i;%s\n", &cep, &log))!=EOF )
+		while( (fscanf(arq,"%s\n", linha))!=EOF )
         {
-            if(buscaCEP == cep){
-                printf ("\n Resultado do CEP : %i, %s \n", cep, &log);
-                break;
-            }
-            else{
-                printf ("\n Não achamos CEP correspondente na base.");
-                break;
-            }
+            a[i] = linha;
+            i++;
         }
-	}
-
-    //Para tempo de leitura e Imprime tempo
-    clock_t end = clock();
-    printf ("\nO Tempo de leitura dos dados gasto foi: %f\n\n", (double)(end - begin) * 1000 / CLOCKS_PER_SEC);
 
     //Cria arvore com os dados de cada linha na variavel a
-	//raiz = criaArvore(a, 0, TAMANHO_ARRAY);
+	raiz = criaArvore(a, 0, TAMANHO_ARRAY);
+
     fclose(arq);
 
-    //Inicia tempo de busca
-    clock_t comeco = clock();
+    printf("Digite sua busca: \n");
+    scanf("%s",&entrada);
 
-    //converte entrada em binario para char e busca na arvore
-    //resultado = arvore_binaria_buscar(raiz,chartobin(entrada));
-
-
-    //Para tempo de busca e Imprime O tempo
-    clock_t fim = clock();
-    printf ("\nO Tempo de busca dos dados gasto foi: %f\n\n", (double)(fim - comeco) * 1000 / CLOCKS_PER_SEC);
+    //converte entrada em binario e busca na arvore
+    resultado = arvore_binaria_buscar(raiz,chartobin(entrada));
 
     //converte resultado binario em string traduzida
-    //printf ("\nResultado da busca: %s", bintochar(resultado));
+    bintochar(resultado);
+
+    //imprime retorno da busca
+    printf("Resultado da busca", resultado);
+    scanf("%s, pressione qualquer tecla para continuar.",&linha);
 
     return 0;
 }
 
+
 //------------------------------------------------------
 // Converte uma string em binario para um valor char
 //------------------------------------------------------
-/*unsigned char bintochar( char* binstr )
+unsigned char bintochar( char* binstr )
 {
     // Retorna o valor char que a string passada como argumento
     // representa. Utiliza a funcao strtoul da biblioteca padrao
@@ -86,16 +65,15 @@ int main(){
 //----------------------------------------------------------
 // Converte um valor do tipo char para uma string binaria
 //----------------------------------------------------------
-char *chartobin ( unsigned char c ){
-    static char bin[CHAR_BIT + 1] = {0};
-    int         i;
+char* chartobin ( unsigned char c ){
+    static char bin[CHAR_BIT + 1] = { 0 };
+    int i;
 
-    for ( i = CHAR_BIT - 1; i >= 0; i-- )
-    {
+    for ( i = CHAR_BIT - 1; i >= 0; i-- ){
         bin[i] = (c % 2) + '0';
         c /= 2;
     }
-
+    // Retorna a string com a representacao binaria do argumento
     return bin;
 }
 
